fecf615eab0ab2419e3f02b85bc214ea959f019f
Sandalf <alfsand97@gmail.com>
2016-03-18 23:17:09 -0700

    Added mission folder

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/partials/missions.html',function(req,res){
 	res.sendFile(__dirname + '/client/js/partials/missions.html');
 });
 
-app.get('/partials/mission-1.html',function(req,res){
-	res.sendFile(__dirname + '/client/js/partials/mission-1.html');
+app.get('/partials/missions/mission-1.html',function(req,res){
+	res.sendFile(__dirname + '/client/js/partials/missions/mission-1.html');
 });
 
-app.get('/partials/mission-2.html',function(req,res){
-	res.sendFile(__dirname + '/client/js/partials/mission-2.html');
+app.get('/partials/missions/mission-2.html',function(req,res){
+	res.sendFile(__dirname + '/client/js/partials/missions/mission-2.html');
 });
 
-app.get('/partials/mission-3.html',function(req,res){
-	res.sendFile(__dirname + '/client/js/partials/mission-3.html');
+app.get('/partials/missions/mission-3.html',function(req,res){
+	res.sendFile(__dirname + '/client/js/partials/missions/mission-3.html');
 });
 
 app.use('/css', express.static(__dirname + '/client/css'));
